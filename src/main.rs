0ffe8bba62b5ecee0bf3842b8802b102cf47c793
nagy nabil <nagynabil651@gmail.com>
2023-07-01 02:31:40 +0300

    run one min after reg

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ use std::io::Error;
 
 // use windows::core::GUID;
 use windows::{
-    core::GUID,
-    Win32::System::{Com::*, Ole::VariantInit, TaskScheduler::*},
+    core::{ComInterface, BSTR, GUID},
+    Win32::{
+        Foundation::VARIANT_TRUE,
+        System::{Com::*, Ole::VariantInit, TaskScheduler::*},
+    },
 };
 
+// create task that run after 2mins
+// based on -> https://learn.microsoft.com/en-us/windows/win32/taskschd/time-trigger-example--c---
 fn main() -> Result<(), Error> {
     unsafe {
         // solve Error: Os { code: -2147467262, kind: Uncategorized, message: "No such interface supported" }
@@ -39,14 +44,88 @@ fn main() -> Result<(), Error> {
         let service: ITaskService =
             CoCreateInstance(&CLSID_TaskService, None, CLSCTX_INPROC_SERVER)?;
 
+        // connect task service
         service.Connect(VariantInit(), VariantInit(), VariantInit(), VariantInit())?;
 
-        let name = service.Connected()?;
-        println!("{}", name.as_bool());
-        let u = service.ConnectedUser()?;
-        println!("🪵 [main.rs:46]~ token ~ \x1b[0;32mu\x1b[0m = {}", u);
+        //  ------------------------------------------------------
+        //  Get the pointer to the root task folder.  This folder will hold the
+        //  new task that is registered.
+        let folder: ITaskFolder = service.GetFolder(&BSTR::from(r"\"))?;
+        let tasks = folder.GetTasks(TASK_ENUM_HIDDEN.0)?;
 
-        // connect to task service
+        let count = tasks.Count()?;
+        println!("Number of tasks in root folder: {count}");
+
+        let task_name = BSTR::from(r"My Task");
+        // if the same task exists remove it
+        // folder.DeleteTask(&task_name, 0)?;
+
+        //  Create the task definition object to create the task.
+        let task = service.NewTask(0)?;
+
+        //  ------------------------------------------------------
+        //  Get the registration info for setting the identification.
+        let reg_info = task.RegistrationInfo()?;
+
+        reg_info.SetAuthor(&BSTR::from("nagy-nabil"))?;
+
+        //  ------------------------------------------------------
+        //  Create the principal for the task - these credentials
+        //  are overwritten with the credentials passed to RegisterTaskDefinition
+        let principal = task.Principal()?;
+
+        //  Set up principal logon type to interactive logon
+        principal.SetLogonType(TASK_LOGON_INTERACTIVE_TOKEN)?;
+
+        //  ------------------------------------------------------
+        //  Create the settings for the task
+        let task_settings = task.Settings()?;
+
+        //  Set setting values for the task.
+        task_settings.SetStartWhenAvailable(VARIANT_TRUE)?;
+
+        //  ------------------------------------------------------
+        //  Get the trigger collection to insert the time trigger.
+        let trigger_collection = task.Triggers()?;
+
+        //  Add the time trigger to the task.
+        let trigger = trigger_collection.Create(TASK_TRIGGER_TIME)?;
+        let time_trigger = trigger.cast::<ITimeTrigger>()?;
+        time_trigger.SetId(&BSTR::from(r"Trigger1"))?;
+
+        time_trigger.SetEndBoundary(&BSTR::from(r"2024-05-02T08:00:00"))?;
+
+        //  Set the task to start at a certain time. The time
+        //  format should be YYYY-MM-DDTHH:MM:SS(+-)(timezone).
+        //  For example, the start boundary below
+        //  is January 1st 2005 at 12:05
+        time_trigger.SetStartBoundary(&BSTR::from(r"2005-01-01T12:05:00"))?;
+
+        //  ------------------------------------------------------
+        //  Add an action to the task. This task will execute notepad.exe.
+
+        //  Get the task action collection pointer.
+        let action_collection = task.Actions()?;
+
+        //  Create the action, specifying that it is an executable action.
+        let action = action_collection.Create(TASK_ACTION_EXEC)?;
+        let exec_action = action.cast::<IExecAction>()?;
+        //  Set the path of the executable to notepad.exe.
+        exec_action.SetPath(&BSTR::from(r"C:\Program Files\LGHUB\lghub.exe"))?;
+
+        //  ------------------------------------------------------
+        //  Save the task in the root folder.
+        let reged_tasl = folder.RegisterTaskDefinition(
+            &task_name,
+            &task,
+            6,
+            VariantInit(),
+            VariantInit(),
+            TASK_LOGON_INTERACTIVE_TOKEN,
+            VariantInit(),
+        )?;
+
+        println!("\n Success! Task successfully registered. ");
     }
     return Ok(());
 }
